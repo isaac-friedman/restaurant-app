6895092357d4248784d710166196d900c1966530
Isaac Friedman <isaac@isaac-friedman.com>
2019-06-27 20:35:56 +0000

    Database setup files

---
+++ database_setup.py
@@ -0,0 +1,26 @@
+import sys
+
+from sqlalchemy import Column, Numeric, String, Integer, ForeignKey, create_engine
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
+
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
+    description = Column(String(280))
+    course = Column(String(10))
+    price = Column(String(6), nullable = False)
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.create_all(engine)
